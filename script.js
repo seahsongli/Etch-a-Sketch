b780817efba79ad7dfa4766aa2f3b354d6318a0e
Philedo <seahsongli@gmail.com>
2022-07-19 21:28:44 +0800

    Change hovering into a declared function. Add a function to change grid size. Add a button to change grid size. Insert interpolation for arrangement of rows and cols.

--- script.js
+++ script.js
@@ -1,23 +1,59 @@
-const container = document.querySelector("#container");
-container.classList.add("container");
-
-function makeRows(rows, cols){
+function populateGrid(rows, cols){
+    let wholeGrid = document.querySelector("#container");
+  
     for(i=0;i < rows*cols;i ++){
         let cell= document.createElement('div');
-        cell.setAttribute("id", "cell")
+        cell.setAttribute("id", "cell");
         container.append(cell);
-        
     }
-    
+    wholeGrid.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
+    wholeGrid.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+    hovering()
 }
-const rows = 16;
-const cols = 16;
-makeRows(rows,cols);
+   
+
+// const container = document.querySelector("#container");
+// container.classList.add("container");
+
+// function makeRows(rows, cols){
+//     for(i=0;i < rows*cols;i ++){
+//         let cell= document.createElement('div');
+//         cell.setAttribute("id", "cell")
+//         container.append(cell);
+        
+//     }
+    
+// }
+populateGrid(16,16);
 
 //add event to all cells when hovering
-let cells = document.querySelectorAll("#cell")
-cells.forEach(cells=>cells.addEventListener("mouseenter", function(e){
-    cells.classList.add("playing");
-}));
+
+function hovering(){
+    let cells = document.querySelectorAll("#cell")
+    cells.forEach(cells=>cells.addEventListener("mouseenter", function(e){
+        cells.classList.add("playing");
+}
+))};
+//To set button for changing of grid size
+let button1 = document.querySelector("#button1");
+button1.addEventListener("click", changeSize);
+button1.innerHTML = "Change Grid Size"
+
+function changeSize(rows,cols){
+    let newRow = prompt("What is your desired number of rows/columns?")
+    if (newRow > 100){
+        alert("Maximum number of rows you can have is 100. Please input a smaller number!")
+    }
+    else{
+        let cells = document.querySelectorAll("#cell")
+        cells.forEach((div) =>div.remove());
+        let newCols = newRow;
+        hovering()
+        return populateGrid(newRow,newCols);
+
+    }
+   
+}
+
 
 
