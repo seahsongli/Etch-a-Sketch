0aedd09aa22ba7bb85145d3b53b179da6ae596a6
Philedo <seahsongli@gmail.com>
2022-07-20 14:38:32 +0800

    Add drag function to individual squares.

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ function populateGrid(rows, cols){
         let cell= document.createElement('div');
         cell.setAttribute("id", "cell");
         container.append(cell);
+        cell.addEventListener("mousedown", hovering);
+        cell.addEventListener("mouseover", hovering);
     }
     wholeGrid.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
     wholeGrid.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
-    hovering()
+    
 }
    
 
@@ -28,16 +30,29 @@ populateGrid(16,16);
 
 //add event to all cells when hovering
 
-function hovering(){
-    let cells = document.querySelectorAll("#cell")
-    cells.forEach(cells=>cells.addEventListener("mouseenter", function(e){
-        cells.classList.add("playing");
-}
-))};
-//To set button for changing of grid size
+function hovering(event){
+    const cells = document.querySelectorAll("#cell");
+    cells.forEach(cells => cells.addEventListener("dragover", function (e){
+        event.preventDefault();
+        cells.classList.add("playing")
+    }));
+};
+
+// function hovering(event){
+//     const cells = document.querySelectorAll("#cell");
+//     if (event.type == "mouseover" && event.type !== "mousedown"){
+//         return;
+//     }
+//     else {
+//         cells.forEach(cells => cells.addEventListener("mouseover", function(e){
+//             cells.classlist.add("playing");
+//         }))
+//         };
+//     }
+//To set button1 for changing of grid size
 let button1 = document.querySelector("#button1");
 button1.addEventListener("click", changeSize);
-button1.innerHTML = "Change Grid Size"
+button1.innerHTML = "Change Grid Size";
 
 function changeSize(rows,cols){
     let newRow = prompt("What is your desired number of rows/columns?")
@@ -45,15 +60,25 @@ function changeSize(rows,cols){
         alert("Maximum number of rows you can have is 100. Please input a smaller number!")
     }
     else{
-        let cells = document.querySelectorAll("#cell")
+        let cells = document.querySelectorAll("#cell");
         cells.forEach((div) =>div.remove());
         let newCols = newRow;
-        hovering()
+        hovering(event)
         return populateGrid(newRow,newCols);
 
     }
    
-}
+};
+
+//Reset button for grid
+let button2 = document.querySelector("#button2");
+button2.addEventListener("click",reset);
+button2.innerHTML = "Reset";
+function reset(){
+    let cells = document.querySelectorAll("#cell");
+    cells.forEach(cell => cell.classList.remove('playing'));
+};
 
 
+let button3 = document.querySelector("#button3");
 
